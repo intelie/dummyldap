4357abb891ef88fb91f9195f4484bf6d31352ca8
Juan Lopes <me@juanlopes.net>
2014-12-11 16:24:55 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Installation
+
+```sh
+mkdir dummyldap && cd $_
+curl -s intelie.github.io/dummyldap | sh
+```
+
+Installing another version:
+
+```
+curl -s intelie.github.io/dummyldap | sh -s 0.2
+```
---
+++ src/main/java/net/intelie/dummyldap/Main.java
@@ -0,0 +1,68 @@
+package net.intelie.dummyldap;
+
+import org.apache.directory.server.core.DefaultDirectoryService;
+import org.apache.directory.server.core.DirectoryService;
+import org.apache.directory.server.core.partition.Partition;
+import org.apache.directory.server.core.partition.impl.btree.jdbm.JdbmPartition;
+import org.apache.directory.server.ldap.LdapService;
+import org.apache.directory.server.protocol.shared.SocketAcceptor;
+import org.apache.directory.server.protocol.shared.store.LdifFileLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String... args) throws Exception {
+
+        int port = 1389;
+        if (args.length >= 1)
+            port = Integer.parseInt(args[0]);
+
+        boolean first = false;
+
+        File workingDirectory = new File("./ldapdata");
+        if (!workingDirectory.exists())
+            first = true;
+
+        workingDirectory.mkdirs();
+
+        Partition partition = new JdbmPartition();
+        partition.setId("ou=Users");
+        partition.setSuffix("dc=example,dc=com");
+
+        DirectoryService directory = new DefaultDirectoryService();
+        directory.setWorkingDirectory(workingDirectory);
+        directory.addPartition(partition);
+        directory.setShutdownHookEnabled(false);
+
+        LdapService server = new LdapService();
+        server.setSocketAcceptor(new SocketAcceptor(null));
+        server.setIpPort(port);
+        server.setDirectoryService(directory);
+
+        directory.startup();
+        server.start();
+
+        if (first) {
+            File startup = new File("./startup.ldif");
+            if (!startup.exists()) {
+                startup.getParentFile().mkdirs();
+                try (InputStream input = Main.class.getResourceAsStream("/test.ldif")) {
+                    Files.copy(input, startup.toPath());
+                }
+            }
+            LdifFileLoader ldif = new LdifFileLoader(directory.getAdminSession(), startup.getPath());
+            ldif.execute();
+        }
+        LOG.info("*******************");
+        LOG.info("ADMIN: uid=admin,ou=system (default pass: 'secret')");
+        LOG.info("*******************");
+
+    }
+
+}
